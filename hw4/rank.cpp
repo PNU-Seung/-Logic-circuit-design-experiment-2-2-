761f66818defd578110aba7007591dcdb83c2f62
PNU-Seung <38664481+PNU-Seung@users.noreply.github.com>
2018-09-28 16:04:49 +0900

    size_reduce_Version

--- hw4/rank.cpp
+++ hw4/rank.cpp
@@ -3,93 +3,26 @@
 #include <algorithm>
 #include <fstream>
 using namespace std;
-
-class assignment {
-	int index;
-	int rank;
-	vector<int> score;
-public:
-	assignment() {
-		index = -1;
-		rank = -1;
-	}
-	void setAssignment(int num) {
-		index = num;
-		rank = num+1;
-	}
-	void inputScore(int _score) {
-		score.push_back(_score);
-	}
-	int getRank(void) const {
-		return rank;
-	}
-	int getindex(void) const {
-		return index+1;
-	}
-	void sortArray(void) {
-		sort(score.begin(), score.end());
-	}
-	friend bool compare(class assignment c1, class assignment c2); // c1< c2 return bool
-	friend void swapRank(class assignment& c1, class assignment& c2);
-};
-
-int main(void) {
-
-	int N;
-	ifstream fp;
-	fp.open("rank.inp");
-	fp >> N;
-	class assignment* c = new assignment[N];
-	int num;
-	for (int i = 0; i < N; i++) {
-		c[i].setAssignment(i);
-		do {
-			fp >> num;
-			if (num == -1)
-				break;
-			c[i].inputScore(num);
-		} while (true);
-		c[i].sortArray();
-	}
-	fp.close();
-
-	class assignment temp;
-	for(int i = 0; i < N-1; i++)
-		for (int j = N-1-1; j >= 0+i; j--) {
-			if (compare(c[j], c[j + 1])) { // c1 < c2
-				temp = c[j];
-				c[j] = c[j + 1];
-				c[j + 1] = temp;
-				swapRank(c[j], c[j + 1]);
-			}
-		}
-	ofstream ofp;
-	ofp.open("rank.out");
-	for (int i = 0; i < N; i++)
-		ofp << c[i].getindex() << endl;
-	ofp.close();
-
-	delete[] c;
-	return 0;
-}
-bool compare(class assignment c1, class assignment c2) { // c1< c2 return bool;
-	if (c1.score.size() < c2.score.size())
-		return true;
-	else if (c1.score.size() > c2.score.size())
-		return false;
-	else if (c1.score.size() == c2.score.size()) {
-		if (c1.score < c2.score)
-			return true;
-		else if (c1.score > c2.score)
-			return false;
-		else if (c1.score == c2.score)
-			return (c1.index + 1) > (c2.index + 1);
-		else
-			exit(-1);
-	}
-}
-void swapRank(class assignment& c1, class assignment& c2) {
-	int temp = c1.rank;
-	c1.rank = c2.rank;
-	c2.rank = temp;
-}
+struct assignment{int index;vector<int> score;assignment(){}};
+bool compare(assignment c1,assignment c2){
+if(c1.score.size()<c2.score.size())return 1;
+else if(c1.score.size()>c2.score.size())return 0;
+else if(c1.score.size()==c2.score.size()){
+if(c1.score<c2.score)return 1;
+else if(c1.score>c2.score)return 0;
+else if(c1.score==c2.score)return(c1.index)>(c2.index);}}
+int main(void){int N;ifstream fp("rank.inp");fp>>N;
+assignment* c=new assignment[N];int num;
+for(int i=0;i<N;i++){c[i].index=i+1;
+while(fp>>num&&num!=-1){
+c[i].score.push_back(num);}
+sort(c[i].score.begin(),c[i].score.end());}fp.close();assignment temp;
+for(int i=0;i<N-1;i++)
+for(int j=N-2;j>=i;j--){
+if(compare(c[j],c[j+1])){
+temp=c[j];c[j]=c[j+1];c[j+1]=temp;}}
+ofstream ofp("rank.out");
+for(int i=0;i<N;i++)ofp<<c[i].index<<endl;
+ofp.close();
+delete[] c;
+return 0;}
