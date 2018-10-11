46b6d446bb7e5c32fe49fae9cf43eeec8293e84d
PNU-Seung <38664481+PNU-Seung@users.noreply.github.com>
2018-10-11 21:34:15 +0900

    for k1>k2 case

--- hw5/function.cpp
+++ hw5/function.cpp
@@ -7,29 +7,23 @@
 #include <utility>
 #include <queue>
 
-using std::vector;
-using std::string;
-using std::ifstream;
-using std::ofstream;
-using std::stack;
-using std::pair;
-using std::queue;
+using namespace std;
 
 vector<string> functionStatement;
-queue<pair<string,string>> statement;
+queue< pair<string,string> > statement;
 
-vector<vector<string>> function;
+vector< vector<string> > function;
 
 int callingFunction(vector<string> _function);
 vector<string>& findFunction(string ch);
 
 int main(void) {
 	int N, k1, k2;
-	ifstream ifp("1.inp");
+	ifstream ifp("3.inp");
 	ifp >> N, ifp >> k1, ifp >> k2;
 	string str;
 	vector<string> vecstr;
-	
+
 
 	for (int i = 0; i < N; i++) {
 		vecstr = vector<string>();
@@ -45,22 +39,32 @@ int main(void) {
 		ofp << "DEADLOCK" << std::endl;
 		return 0;
 	}
+	pair<string, string> k1str;
+	pair<string, string> k2str;
 	int i = 0;
 	while(i < statement.size()) {
-		if (i == k1-1 || i == k2-1)
-			ofp << statement.front().first << "-" << statement.front().second << std::endl;
+		if (i == k1-1)
+			k1str = std::make_pair(statement.front().first,statement.front().second);
+		if (i == k2-1)
+			k2str = std::make_pair(statement.front().first, statement.front().second);
 		statement.pop();
 		i++;
 	}
 	if (i <= k1 - 1)
 		ofp << "NONE" << std::endl;
+	else
+		ofp << k1str.first << "-" << k1str.second << std::endl;
+
 	if (i <= k2 - 1)
 		ofp << "NONE" << std::endl;
+	else
+		ofp << k2str.first << "-" << k2str.second << std::endl;
+
 	ofp.close();
 	return 0;
 }
 vector<string>& findFunction(string ch) {
-	for (vector<vector<string>>::iterator iter = function.begin(); iter != function.end(); iter++) {
+	for (vector< vector<string> >::iterator iter = function.begin(); iter != function.end(); iter++) {
 		if ((*iter).front() == ch)
 			return (*iter);
 	}
@@ -72,11 +76,11 @@ int callingFunction(vector<string> _function) {
 		if ((*iter) == _function.front())
 			return -1;
 	}
-	
+
 	functionStatement.push_back(_function.front());
 
 	for (vector<string>::iterator iter = _function.begin() + 1; iter != _function.end(); iter++) {
-		if (isupper((*iter).front())){
+		if (isupper( (*iter).front() )){
 			if (callingFunction(findFunction((*iter))) == -1)
 				return -1;
 		}
@@ -84,4 +88,5 @@ int callingFunction(vector<string> _function) {
 			statement.push(pair<string, string>((*_function.begin()),(*iter)));
 	}
 	functionStatement.pop_back();
+	return 0;
 }
