b4d73cdd4b26ba2705200d7b588efa1c8beb5871
PNU-Seung <38664481+PNU-Seung@users.noreply.github.com>
2018-11-10 16:58:39 +0900

    ver0.2

--- hw9/log.cpp
+++ hw9/log.cpp
@@ -2,22 +2,26 @@
 #include <fstream>
 #include <list>
 #include <vector>
+#include <utility>
+#include <algorithm>
+#include <numeric>
 
 using std::list;
+using std::pair;
+using std::vector;
 
 struct Log {
-	std::vector<int> user;
+	vector<int> user;
 	int ts;
 	int te;
-
 };
 
 namespace timeSlot {
 	void calculator(int user, int start, int end);
-	void splice(int start, int end);
+	void splice(list<Log>::iterator iter, int start, int end);
 	void addNewslot(int user, int start, int end);
 	void addUser(int user, int start, int end);
-	bool checkOverLap(int start, int end);
+	vector<pair<int,int>> checkOverLap(int start, int end);
 }
 
 list<Log> serverLog;
@@ -54,21 +58,85 @@ int main(void) {
 
 void timeSlot::calculator(int user, int start, int end) {
 
+
+
 }
 
-void timeSlot::splice(int start, int end)
+void timeSlot::splice(list<Log>::iterator iter, int start, int end)
 {
+	if (start > iter->ts) {
+		Log data;
+		data.user = iter->user;
+		data.ts = start;
+		data.te = end;
+
+		iter->ts = end+1;
+
+		serverLog.insert(iter, data);
+	}
+	else if (end < iter->te) {
+		Log data;
+		data.user = iter->user;
+		data.ts = start;
+		data.te = end;
+
+		iter->ts = end+1;
+
+		serverLog.insert(iter, data);
+
+	}
 }
 
 void timeSlot::addNewslot(int user, int start, int end)
 {
+	Log data;
+	data.user.push_back(user);
+	data.ts = start;
+	data.te = end;
+
+	for (list<Log>::iterator iter = serverLog.begin(); iter != serverLog.end(); iter++) {
+		if (start < iter->ts) {
+			serverLog.insert(iter, data);
+			break;
+		}
+	}
 }
 
 void timeSlot::addUser(int user, int start, int end)
 {
+	for (Log iter : serverLog) {
+		if (iter.ts == start && iter.te == end) {
+			iter.user.push_back(user);
+			sort(iter.user.begin(), iter.user.end());
+			break;
+		}
+	}
 }
 
-bool timeSlot::checkOverLap(int start, int end)
+vector<pair<int,int>> timeSlot::checkOverLap(int start, int end)
 {
-	return false;
+	vector<pair<int, int>> overlap;
+	vector<int> num;
+	vector<int>::iterator overLapStart, overLapEnd;
+	for (const Log iter : serverLog) {
+		
+		num.resize(end - start + 1);
+		std::iota(num.begin(), num.end(), start);
+		overLapStart = std::find(num.begin(), num.end(), iter.ts); overLapEnd =std::find(num.begin(), num.end(), iter.te);
+		
+		if (overLapStart == num.end() && overLapEnd != num.end()){
+			overlap.emplace_back(start, iter.te);
+			start = iter.te + 1;
+		}
+		else if (overLapStart != num.end() && overLapEnd == num.end()) {
+			overlap.emplace_back(iter.ts, end);
+			break;
+		}
+		else if (overLapStart != num.end() && overLapEnd != num.end()) {
+			overlap.emplace_back(iter.ts, iter.te);
+			start = iter.te + 1;
+		}
+	}
+
+	return overlap;
 }
