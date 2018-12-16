3aab533e17e8e03be745c66e18734db8e9b022ef
PNU-Seung <38664481+PNU-Seung@users.noreply.github.com>
2018-12-16 23:35:54 +0900

    .

--- hw4/rank.cpp
+++ hw4/rank.cpp
@@ -1,175 +1,42 @@
-#include <bits/stdc++.h>
-
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <fstream>
 using namespace std;
-
-typedef struct Node
-{
-	string name;
-	vector<Node *>child;
-	int depth = 0;
-	int child_cnt = 0;
-}Node;
-
-Node *newNode(string name)
-{
-	Node *temp = new Node;
-	temp->name = name;
-	return temp;
+struct assignment {
+	int index;
+	vector<int> score;
+	assignment() {}
+};
+bool compare(assignment c1, assignment c2) {
+	if (c1.score.size() == c2.score.size()) {
+		if (c1.score == c2.score)return c1.index > c2.index;
+		else return c1.score < c2.score ? 1 : 0;
+	}
+	else return c1.score.size() < c2.score.size() ? 1 : 0;
 }
-
-vector<Node*> find_Leaves(Node *node) {
-	deque<Node *> leaf;
-	vector<Node *> vec;
-	if (node->child.size() == 0) {
-		return vec;
-	}
-	for (int i = 0; i < node->child.size(); i++) {
-		leaf.push_back(node->child[i]);
-	}
-	while (leaf.size() != 0) {
-		Node * n = leaf.front();
-		if (n->child.size() == 0) vec.push_back(n);
-		else {
-			for (int j = 0; j < n->child.size(); j++) {
-				leaf.push_back(n->child[j]);
-			}
-		}
-		leaf.pop_front();
-	}
-	return vec;
-}
-
-vector<Node*> find_depth(Node *node) {
-	deque<Node *> leaf;
-
-	for (int i = 0; i < node->child.size(); i++) {
-		node->child[i]->depth++;
-		leaf.push_back(node->child[i]);
-	}
-	while (leaf.size() != 0) {
-		Node *n = leaf.front();
-		if (n->child.size() != 0) {
-			for (int j = 0; j < n->child.size(); j++) {
-				n->child[j]->depth = n->depth + 1;
-				leaf.push_back(n->child[j]);
-			}
-		}
-		else {
-			for (int j = 0; j < n->child.size(); j++) {
-				n->child[j]->depth = n->depth + 1;
-			}
-		}
-		leaf.pop_front();
-	}
-}
-
-void find_Child(Node *node) {
-	deque<Node *> leaf;
-	int cnt = 0;
-	if (node->child.size() == 0) {
-		return;
-	}
-
-	for (int i = 0; i < node->child.size(); i++) {
-		leaf.push_back(node->child[i]);
-		cnt++;
-	}
-	while (leaf.size() != 0) {
-		Node * n = leaf.front();
-		if (n->child.size() == 0) {
-
-		}
-		else {
-			for (int j = 0; j < n->child.size(); j++) {
-				leaf.push_back(n->child[j]);
-				cnt++;
-			}
-		}
-		leaf.pop_front();
-	}
-	node->child_cnt = cnt;
-}
-
-bool CompareNode(Node *a, Node *b)
-{
-	if (a->child_cnt > b->child_cnt) {
-		return true;
-	}
-	else if (a->child_cnt == b->child_cnt) {
-		if (a->depth < b->depth) {
-			return true;
-		}
-		else if (a->depth == b->depth) {
-			if ((a->name).compare(b->name) < 0) {
-				return true;
-			}
-			else return false;
-		}
-		else return false;
-	}
-	else return false;
-}
-
-int main()
-{
-	ifstream in("org.inp");
-	ofstream out("org.out");
-
+int main(void) {
+	int N; ifstream fp("rank.inp");
 	int N;
-	in >> N;
-
-	vector<Node *> nodeList;
-
-	for (int i = 0; i < N - 1; i++) {
-		string boss;
-		string person;
-		in >> person >> boss;
-		if (i == 0) {
-			Node * n1 = newNode(boss);
-			Node * n2 = newNode(person);
-			nodeList.push_back(n1);
-			nodeList.push_back(n2);
-			n1->child.push_back(n2);
-		}
-		else {
-			Node *n1;
-			Node* n2;
-			bool chk1 = false;
-			bool chk2 = false;
-			for (int j = 0; j < nodeList.size(); j++) {
-				if (nodeList[j]->name == person) {
-					chk1 = true;
-					n1 = nodeList[j];
-				}
-				else if (nodeList[j]->name == boss) {
-					chk2 = true;
-					n2 = nodeList[j];
-				}
-			}
-			if (chk1 == false) {
-				n1 = newNode(person);
-				nodeList.push_back(n1);
-			}
-			if (chk2 == false) {
-				n2 = newNode(boss);
-				nodeList.push_back(n2);
-			}
-			n2->child.push_back(n1);
-		}
+	ifstream ip("rank.inp");
+	ip >> N;
+	assignment* c = new assignment[N];
+	int a;
+	for (int i = 0; i < N; i++) {
+		c[i].index = i + 1;
+		while (ip >> a && a != -1) { c[i].score.push_back(a); }
+		sort(c[i].score.begin(), c[i].score.end());
 	}
-	Node * root;
-	for (int i = 0; i < nodeList.size(); i++) {
-		find_Child(nodeList[i]);
-		if (nodeList[i]->child_cnt == (nodeList.size() - 1)) {
-			root = nodeList[i];
+	ip.close();
+	assignment temp;
+	for (int i = 0; i < N - 1; i++)
+		for (int j = N - 2; j >= i; j--) {
+			if (compare(c[j], c[j + 1]))
+				temp = c[j], c[j] = c[j + 1], c[j + 1] = temp;
 		}
-	}
-	find_depth(root);
-	sort(nodeList.begin(), nodeList.end(), CompareNode);
-	for (int i = 0; i < nodeList.size(); i++) {
-		out << nodeList[i]->name << " ";
-	}
-
-	in.close();
-	out.close();
+	ofstream op("rank.out");
+	for (int i = 0; i < N; i++)op << c[i].index << endl;
+	op.close();
+	delete[] c;
+	return 0;
 }
